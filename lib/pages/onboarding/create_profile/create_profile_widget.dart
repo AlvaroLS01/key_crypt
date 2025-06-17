024e99ce181bc96ee19c58195538741fe5c14d10
Chemo04 <josemariagonpen@gmail.com>
2025-06-18 00:52:18 +0200

    fix: import actions in create profile

--- lib/pages/onboarding/create_profile/create_profile_widget.dart
+++ lib/pages/onboarding/create_profile/create_profile_widget.dart
@@ -2,6 +2,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/index.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
 import 'create_profile_model.dart';
 export 'create_profile_model.dart';
